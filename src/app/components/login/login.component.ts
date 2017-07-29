1574746b535f0ed6291d724b2de966959843eb61
bigal41 <clarkie4114@gmail.com>
2017-07-29 13:57:26 -0400

    Adding some structure.
    Adding a skeleton structure to the website.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,18 +5,46 @@ import {AuthService} from '../../services/auth.service';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   username: string;
   password: string;
+  name: string;
+  pokemonImg: string;
+  url: string;
 
-  constructor( private _authService :AuthService ){ }
+  constructor( private _authService :AuthService ){
+
+    let currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+
+    if( currentUser != null )
+      this.name = currentUser.user.name;
+
+  }
 
   login() {
     this._authService.login(this.username, this.password).subscribe( data => console.log(data),
                                                                      err => console.log(err),
-                                                                     () => { }  );
+                                                                     () => { this.name = JSON.parse(sessionStorage.getItem('currentUser')).user.name; }  );
+  }
+
+  //TODO: Move to pokemon service.
+
+  pokemonImage() {
+    this._authService.pokemonImage().subscribe(
+      data => {
+        this.pokemonImg = data;
+        this.url = 'data:image/png;base64,' + this.pokemonImg;
+      },
+      err => console.log(err),
+      () => { }
+    );
+  }
+
+  logout(){
+    this._authService.logout();
+    this.name = '';
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,15 +8,50 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AuthService {
 
-  private _apiAddress = "https://ballaboveall.ralexclark.ca:8080/api";
+  //private _apiAddress = "https://ballaboveall.ralexclark.ca:8080/api";
+  private _apiAddress = "http://localhost:8181/api"
+  private  _token: string;
   private _options = {
     post: new RequestOptions({headers: new Headers({ 'Content-Type': 'application/json'})})
   }
 
-  constructor(private _http :Http){}
+  get token() { return this._token }
+
+  constructor(private _http :Http){
+    var currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+    this._token = currentUser && currentUser.token;
+  }
 
   login(username, password){
-    return this._http.post( this._apiAddress + '/login', {username: username, password: password} ,this._options ).map(res => res.json())
+    return this._http.post( this._apiAddress + '/login', {username: username, password: password} ,this._options )
+      .map((response: Response) => {
+
+        let token = response.json() && response.json().token;
+        let user = response.json() && response.json().user;
+
+        if(token) {
+           this._token = token;
+
+           sessionStorage.setItem('currentUser', JSON.stringify({user: user, token: token}));
+
+           return true;
+        }
+        else {
+          return false;
+        }
+      })
+  }
+
+  logout():void{
+    this._token = null;
+    sessionStorage.removeItem('currentUser');
+  }
+
+  pokemonImage() {
+    return this._http.get( this._apiAddress + '/pokemonimage')
+      .map((response: Response) => {
+        return response.text();
+      })
   }
 
 }
